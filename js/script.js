d6b5e966ed9eb9346088c34725f3834598672760
Ddouglas20 <DeQuan.Douglas2020@gmail.com>
2025-07-09 23:21:20 -0400

    Working Website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello, Github Pages!")
+document.addEventListener('DOMContentLoaded', function () {
+    const contactButton = document.getElementById('h3-contact');
+    const footer = document.getElementById('footer');
+
+    contactButton.addEventListener('click', function () {
+      footer.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+
+
